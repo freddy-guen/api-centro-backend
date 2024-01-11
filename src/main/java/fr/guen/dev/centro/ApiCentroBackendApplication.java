38c724bf8c85dae27bb2e77aed8f4fe49a2d3342
Freddy Guen <gfreddy@yahoo.fr>
2024-01-11 17:21:16 +0100

    Initialisation du projet

---
+++ src/main/java/fr/guen/dev/centro/ApiCentroBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.guen.dev.centro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCentroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCentroBackendApplication.class, args);
+	}
+
+}
