78ef91f2430eb40a1018a3dc68df764a3732bc08
Freddy Guen <gfreddy@yahoo.fr>
2024-01-30 13:27:04 +0100

    Ajout de l'endpoint de deconnexion

--- src/main/java/fr/guen/dev/centro/controller/UserAuthenticationController.java
+++ src/main/java/fr/guen/dev/centro/controller/UserAuthenticationController.java
@@ -15,10 +15,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +28,7 @@ public class UserAuthenticationController {
     private final UserAuthenticationService userAuthenticationService;
     private final RefreshTokenService refreshTokenService;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponseDTO> register(@Valid @RequestBody RegisterRequestDTO requestDTO)
@@ -65,6 +66,32 @@ public class UserAuthenticationController {
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, newJwtCookie.toString())
+                .build();
+    }
+
+    @GetMapping("/info")
+    public Authentication getAuthentication(@RequestBody LoginRequestDTO request){
+        return  authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request){
+
+        String refreshToken = refreshTokenService.getRefreshTokenFromCookie(request);
+
+        //Suppression du refreshToken de la base
+        if(refreshToken != null) {
+            refreshTokenService.deleteByToken(refreshToken);
+        }
+
+        //Suppression des cookies
+        ResponseCookie jwtCookie = jwtService.getCleanJwtCookie();
+        ResponseCookie refreshTokenCookie = refreshTokenService.getCleanRefreshTokenCookie();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,jwtCookie.toString())
+                .header(HttpHeaders.SET_COOKIE,refreshTokenCookie.toString())
                 .build();
     }
 }
